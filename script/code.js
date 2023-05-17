8d060e573404508cf997558784693b037c0131c8
Codi-K <codikader7@gmail.com>
2023-05-17 10:16:16 +0200

    day 4 exercise

---
+++ script/code.js
@@ -0,0 +1,29 @@
+// Question 1 
+
+let games = {
+    fps: "Apex Legends",
+    br: "PubG"
+
+};
+
+
+
+// Question 2
+
+let pcGames = {
+    fps: "Rust",
+    br: "SuperPeople"
+};
+
+// Question 3
+
+console.log(games);
+console.log(pcGames);
+
+// Question 4
+
+let allGames = [games, pcGames]
+
+// Question 5
+
+console.log(allGames);
